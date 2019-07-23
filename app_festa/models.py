1570c5617980718d18441e65b23f5e0197ef1d49
hyeji0503 <hyeji9605@gmail.com>
2019-07-23 13:28:41 +0900

    festa_ready_crud

--- app_festa/models.py
+++ app_festa/models.py
@@ -14,3 +14,24 @@ class Festa(models.Model) :
 
     def __str__(self) :
         return self.name
+
+
+class Accompany(models.Model) : #festa_ready의 동행구하는 게시판
+    title = models.CharField(max_length=200) #글제목
+    writer = models.CharField(max_length=200) #작성자
+    area = models.CharField(max_length=200) #지역
+    description = models.TextField() #내용
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Ticket(models.Model): #festa_ready의 티켓 양도 교환 게시판
+    title = models.CharField(max_length=200) #제목
+    writer = models.CharField(max_length=200) #작성자
+    deal_type = models.CharField(max_length=200) #양도/교환
+    description = models.TextField() #내용
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
