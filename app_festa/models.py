77170f8f4bb0726f7d4d8d8a57c4dd4a6296acd2
jinseock95 <tz5@naver.com>
2019-07-27 13:46:28 +0900

    comments

--- app_festa/models.py
+++ app_festa/models.py
@@ -67,3 +67,23 @@ class Team(models.Model):
     def summary(self):
         return self.body2[:100]
 
+
+class Commentn(models.Model):
+    writer_now = models.CharField(max_length=200)
+    content_now = models.TextField()
+    now = models.ForeignKey(Now, on_delete=models.CASCADE)
+
+class Commentt(models.Model):
+    writer_team = models.CharField(max_length=200)
+    content_team = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+##집가자 게시판
+
+class Home(models.Model) : #festa_ready의 동행구하는 게시판
+    title = models.CharField(max_length=200) #글제목
+    writer = models.CharField(max_length=200) #작성자
+    region = models.CharField(max_length=200) #지역
+    body = models.TextField() #내용
+    pub_date = models.DateTimeField('date published')
+
--- app_festa/views.py
+++ app_festa/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Festa
 from .models import Accompany, Ticket
-from .models import Now, Team
+from .models import Now, Team, Commentn, Commentt, Home
 import datetime
+from django.utils import timezone
 
 # Create your views here.
 
@@ -173,104 +174,161 @@ def delete_ticket(request,ticket_id):
 
 
 def now_staff(request):
-    return render(request, 'festa_now/staff.html')
+    return render(request, 'festa_now/staff/staff.html')
 
 def now_audience(request):
-    return render(request, 'festa_now/audience.html')
+    return render(request, 'festa_now/audience/audience.html')
 
-##now_audience_festnow게시판
+## festa.now_audience_festnow게시판
 def now_now(request):
     nows = Now.objects
-    return render(request, 'festa_now/now.html', {'nows': nows})
+    return render(request, 'festa_now/audience/now.html', {'nows': nows})
 
 def new_now(request):
-    return render(request, 'festa_now/new_now.html')    
+    return render(request, 'festa_now/audience/new_now.html')
+
+def detail_now(request, now_id):
+    now = Now.objects.get(pk=now_id)
+    comments_list = Commentn.objects.filter(now = now_id)
+    return render(request, 'festa_now/audience/detail_now.html', {'now' : now, 'comments':comments_list })    
 
 def delete_now(request, now_id):
-    delete_now = get_object_or_404(Lost, pk=now_id)
-    if delete_now.password == request.GET['password'] :
-        delete_now.delete()
-    else:    
-        return redirect('now')
-    return redirect('now')
+    delete_now = get_object_or_404(Now, pk=now_id)
+    delete_now.delete()
+    return redirect('now_now')
+    
 
 def edit_now(request, now_id):
-    edit_now = get_object_or_404(Lost, pk=now_id)
-    if edit_now.password == request.GET['password'] :
-        return render(request, 'festa_now/eidt_now.html', {'now': edit_now})
-    else:
-        return redirect('now')
+    edit_now = Now.objects.get(pk=now_id)
+    return render(request, 'festa_now/audience/edit_now.html', {'now': edit_now})
+
 
 def update_now(request, now_id):
-    update_now = get_object_or_404(Lost, pk= now_id)
-    update_now.title = request.GET["title"]
-    update_now.writer = request.GET['writer']
-    update_now.body = request.GET['body']
-    update_now.save()
-    return redirect('now')
+        update_now = Now.objects.get(id = now_id)
+        update_now.title = request.POST["title"]
+        update_now.writer = request.POST['writer']
+        update_now.body = request.POST['body']
+        update_now.save()
+        return redirect('now_now')
 
 def create_now(request):
-    now = Now()
-    now.title = request.GET['title']
-    now.body = request.GET['body']
-    now.writer = request.GET['writer']
-    now.password = request.GET['password']
-    now.pub_date = timezone.datetime.now()
-    now.save()
-    return redirect('now')
+    if request.method=="POST":
+        now = Now()
+        now.title = request.POST['title']
+        now.body = request.POST['body']
+        now.writer = request.POST['writer']
+        now.pub_date = timezone.datetime.now()
+        now.save()
+        return redirect('now_now')
+    return render(request, 'festa_now/audience/now.html')
 
-def check_edit_now(request, now_id):
-    return render(request, 'check_edit_now.html', {'now_id': now_id})
-
-def check_delete_now(request, now_id):
-    return render(request, 'check_delete_now.html', {'now_id': now_id})
-
-
-
-
-##now_staff_팀별게시판
+## festa.now_staff_팀별게시판
 def now_team(request):
     teams = Team.objects
-    return render(request, 'festa_now/team.html', {'teams': teams})
+    return render(request, 'festa_now/staff/team.html', {'teams': teams})
 
 def new_team(request):
-    return render(request, 'festa_now/new_team.html')    
+    return render(request, 'festa_now/staff/new_team.html')
+
+def detail_team(request, team_id):
+    team = Team.objects.get(pk=team_id)
+    comments_list = Commentt.objects.filter(team = team_id)
+    return render(request, 'festa_now/staff/detail_team.html', {'team' : team, 'comments': comments_list })    
 
 def delete_team(request, team_id):
-    delete_team = get_object_or_404(Lost, pk=team_id)
-    if delete_team.password2 == request.GET['password'] :
-        delete_team.delete()
-    else:    
-        return redirect('team')
-    return redirect('team')
+    delete_team = get_object_or_404(Team, pk=team_id)
+    delete_team.delete()
+    return redirect('now_team')
 
 def edit_team(request, team_id):
-    edit_team = get_object_or_404(Lost, pk=team_id)
-    if edit_team.password2 == request.GET['password'] :
-        return render(request, 'festa_now/eidt_team.html', {'team': edit_team})
-    else:
-        return redirect('team')
+    edit_team = Team.objects.get(pk=team_id)
+    return render(request, 'festa_now/staff/edit_team.html', {'team': edit_team})
+
 
 def update_team(request, team_id):
-    update_team = get_object_or_404(Lost, pk= team_id)
-    update_team.title2 = request.GET["title"]
-    update_team.writer2 = request.GET['writer']
-    update_team.body2 = request.GET['body']
-    update_team.save()
-    return redirect('now')
+        update_team = Team.objects.get(id = team_id)
+        update_team.title2 = request.POST["title"]
+        update_team.writer2 = request.POST['writer']
+        update_team.body2 = request.POST['body']
+        update_team.save()
+        return redirect('now_team')
 
 def create_team(request):
-    now = Now()
-    now.title2 = request.GET['title']
-    now.body2 = request.GET['body']
-    now.writer2 = request.GET['writer']
-    now.password2 = request.GET['password']
-    now.pub_date2 = timezone.datetime.now()
-    now.save()
-    return redirect('team')
+    if request.method=="POST":
+        team = Team()
+        team.title2 = request.POST['title']
+        team.body2 = request.POST['body']
+        team.writer2 = request.POST['writer']
+        team.pub_date2 = timezone.datetime.now()
+        team.save()
+        return redirect('now_team')
+    return render(request, 'festa_now/staff/team.html')
 
-def check_edit_team(request,   team_id):
-    return render(request, 'check_edit_team.html', {'team_id': team_id})
+## 댓글 
+def comment_now(request, now_id):
+    comment = Commentn()
+    comment.writer_now = request.POST['writer']
+    comment.content_now = request.POST['content']
+    comment.now = get_object_or_404(Now, pk=now_id)
+    comment.save()
+    return redirect('detail_now', now_id)
 
-def check_delete_team(request, team_id):
-    return render(request, 'check_delete_team.html', {'team_id': team_id})
+def comment_team(request, team_id):
+    comment = Commentt()
+    comment.writer_team = request.POST['writer']
+    comment.content_team = request.POST['content']
+    comment.team = get_object_or_404(Team, pk=team_id)
+    comment.save()
+    return redirect('detail_team', team_id)
+
+
+##집가자
+def audience_home(request):
+    homes = Home.objects
+    return render(request, 'festa_now/audience/home.html', {'homes':homes})
+
+def staff_home(request):
+    homes = Home.objects
+    return render(request, 'festa_now/staff/home.html', {'homes':homes})    
+
+def new_home(request):
+    return render(request, 'festa_now/new_home.html')
+
+def detail_home(request, home_id):
+    home = Home.objects.get(pk=home_id)
+    return render(request, 'festa_now/detail_home.html', {'home' : home})   
+
+def delete_home(request, home_id):
+    delete_home = get_object_or_404(Home, pk=home_id)
+    delete_home.delete()
+    return redirect('home')
+
+def edit_home(request, home_id):
+    edit_home = Home.objects.get(pk=home_id)
+    return render(request, 'festa_now/edit_home.html', {'home': edit_home})
+
+
+def update_home(request, home_id):
+        update_home = Home.objects.get(id = home_id)
+        update_home.title = request.POST["title"]
+        update_home.writer = request.POST['writer']
+        update_home.body = request.POST['body']
+        update_home.region = request.POST['region']
+        update_home.save()
+        return redirect('home')
+
+def create_home(request):
+    if request.method=="POST":
+        home = Home()
+        home.title = request.POST['title']
+        home.body = request.POST['body']
+        home.writer = request.POST['writer']
+        home.region = request.POST['region']
+        home.pub_date = timezone.datetime.now()
+        home.save()
+        return redirect('home')
+    return render(request, 'festa_now/deatil_home.html')
+
+
+
+
