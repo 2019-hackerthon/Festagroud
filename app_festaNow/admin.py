7f94d73d12a8a68f807c0f7505db07c8209a73b2
jinseock95 <tz5@naver.com>
2019-07-30 03:02:25 +0900

    login성공

--- app_festaNow/admin.py
+++ app_festaNow/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Now, Team
+from .models import Now, Team, ReservationNum
 
 # Register your models here.
 admin.site.register(Now)
 admin.site.register(Team)
+admin.site.register(ReservationNum)
