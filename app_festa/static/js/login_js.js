0361e0d7071b797ad9964a42022e819773b0f8ec
sungminKim <pinokio0330@gmail.com>
2019-08-05 17:29:25 +0900

    layout modify

--- app_festa/static/js/login_js.js
+++ app_festa/static/js/login_js.js
@@ -28,31 +28,53 @@ function btSearchResize(){
 }
 
 function horizentalEditText(){
-    var visiEditBox = document.getElementById('visible_edit_box');
-    var btWrapper = document.getElementById('bt_wrapper');
+    var visiEditBox = document.getElementsByClassName('visible_edit_box');
+    var btWrapper = document.getElementsByClassName('bt_wrapper');
     var editBoxForm = document.getElementById('edit_box_form');
     var numEditText = document.getElementById('num_editText');
     var certificateBt = document.getElementById('certificateBt');
     
     if(window.innerWidth <= 1150){
-        visiEditBox.style.display = 'block';
-        btWrapper.style.marginLeft = '0%';
+        // visiEditBox.style.display = 'block';
+        // btWrapper.style.marginLeft = '0%';
         editBoxForm.style.width = '100%';
-        btWrapper.style.width = '95%';
+        // btWrapper.style.width = '95%';
         numEditText.style.width = '90%';
         certificateBt.style.width = '10%';
+
+        for(var i2 = 0; i2 < visiEditBox.length; i2++){
+            visiEditBox[i2].style.display = 'block';
+        }
+
+        for(var i = 0; i < btWrapper.length; i++){
+            btWrapper[i].style.marginLeft = '0%';
+            btWrapper[i].style.width = '95%';
+        }
 
         if(window.innerWidth <= 800){
             numEditText.style.width = '150px';
             certificateBt.style.width = '35px';
-            btWrapper.style.width = '187px';
+            // btWrapper.style.width = '187px';
+
+            for(var j = 0; j < btWrapper.length; j++){
+                btWrapper[j].style.width = '187px';
+            }
 
         }
     }
     else{
-        visiEditBox.style.display = 'flex';
-        btWrapper.style.marginLeft = '7%';
-        btWrapper.style.width = '70%';
+        // visiEditBox.style.display = 'flex';
+        // btWrapper.style.marginLeft = '7%';
+        // btWrapper.style.width = '70%';
         editBoxForm.style.width = '80%';
+
+        for(var k = 0; k < btWrapper.length; k++){
+            btWrapper[k].style.marginLeft = '7%';
+            btWrapper[k].style.width = '70%';
+        }
+
+        for(var k2 = 0; k2 < visiEditBox.length; k2++){
+            visiEditBox[k2].style.display = 'flex';
+        }
     }
 }
