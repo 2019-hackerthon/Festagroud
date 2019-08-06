5a1dd295a05c2d2be1f19640b51bc0f0745381dd
sungminKim <pinokio0330@gmail.com>
2019-08-06 11:42:05 +0900

    login form modify

--- app_festa/static/js/login_js.js
+++ app_festa/static/js/login_js.js
@@ -19,12 +19,14 @@ window.onresize = function () {
 
 function btSearchResize(){
     var loginInputBt = document.getElementById('certificateBt');
-    var loginInputText = document.getElementById('num_editText');
+    var loginNumInputText = document.getElementById('num_editText');
+    var loginNameInputText = document.getElementById('name_editText');
 
     var loginBtWidth = loginInputBt.offsetWidth;
 
     loginInputBt.style.height = loginBtWidth + 'px';
-    loginInputText.style.height = loginBtWidth + 'px';
+    loginNumInputText.style.height = loginBtWidth + 'px';
+    loginNameInputText.style.height = loginBtWidth + 'px';
 }
 
 function horizentalEditText(){
@@ -32,15 +34,19 @@ function horizentalEditText(){
     var btWrapper = document.getElementsByClassName('bt_wrapper');
     var editBoxForm = document.getElementById('edit_box_form');
     var numEditText = document.getElementById('num_editText');
+    var nameEditText = document.getElementById('name_editText');
     var certificateBt = document.getElementById('certificateBt');
+
+    editBoxForm.style.width = '80%';
+    numEditText.style.width = '90%';
+    nameEditText.style.width = '90%';
+    certificateBt.style.width = '10%';
     
     if(window.innerWidth <= 1150){
         // visiEditBox.style.display = 'block';
         // btWrapper.style.marginLeft = '0%';
-        editBoxForm.style.width = '100%';
         // btWrapper.style.width = '95%';
-        numEditText.style.width = '90%';
-        certificateBt.style.width = '10%';
+        editBoxForm.style.width = '100%';
 
         for(var i2 = 0; i2 < visiEditBox.length; i2++){
             visiEditBox[i2].style.display = 'block';
@@ -53,8 +59,8 @@ function horizentalEditText(){
 
         if(window.innerWidth <= 800){
             numEditText.style.width = '150px';
+            nameEditText.style.width = '150px';
             certificateBt.style.width = '35px';
-            // btWrapper.style.width = '187px';
 
             for(var j = 0; j < btWrapper.length; j++){
                 btWrapper[j].style.width = '187px';
@@ -63,18 +69,16 @@ function horizentalEditText(){
         }
     }
     else{
-        // visiEditBox.style.display = 'flex';
-        // btWrapper.style.marginLeft = '7%';
-        // btWrapper.style.width = '70%';
-        editBoxForm.style.width = '80%';
-
-        for(var k = 0; k < btWrapper.length; k++){
-            btWrapper[k].style.marginLeft = '7%';
-            btWrapper[k].style.width = '70%';
-        }
 
         for(var k2 = 0; k2 < visiEditBox.length; k2++){
             visiEditBox[k2].style.display = 'flex';
         }
+        for(var k = 0; k < btWrapper.length; k++){
+            btWrapper[k].style.marginLeft = '7%';
+            btWrapper[k].style.width = '70%';
+        }
     }
+
+    btWrapper[0].style.width = (numEditText.offsetWidth+2) + 'px';
+    nameEditText.style.width = '100%';
 }
