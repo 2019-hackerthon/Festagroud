50f6a8ad908fe51779c160a2665199358380ae18
sungminKim <pinokio0330@gmail.com>
2019-08-08 20:13:57 +0900

    festa ready layout

---
+++ app_festa/static/js/festa_ready_js.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    this.fullScreen();
+    this.backgroundResize();
+    this.imgSizeLimit();
+    this.imgResize();
+    this.leftSidebarTextLimit();
+    this.menuDisplay();
+}
+window.onresize = function () {
+    this.fullScreen();
+    this.backgroundResize();
+    this.imgSizeLimit();
+    this.imgResize();
+    this.leftSidebarTextLimit();
+    this.menuDisplay();
+}
+
+function menuDisplay(){
+    var nowMenuView = document.getElementById('ready_menu_wrapper');
+    nowMenuView.style.display = 'block';
+}
