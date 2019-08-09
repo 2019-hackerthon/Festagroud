ddb298d0a8390c9e0bc8c26649f9252ddc7ca96d
sungminKim <pinokio0330@gmail.com>
2019-08-09 14:41:24 +0900

    festa ready main

---
+++ app_festa/static/js/ready_main_js.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    this.fullScreen();
+    this.backgroundResize();
+    this.imgSizeLimit();
+    this.imgResize();
+    this.leftSidebarTextLimit();
+
+    this.festaInfoScroll();
+    this.menuDisplay();
+}
+window.onresize = function () {
+    this.fullScreen();
+    this.backgroundResize();
+    this.imgSizeLimit();
+    this.imgResize();
+    this.leftSidebarTextLimit();
+
+    this.festaInfoScroll();
+    this.menuDisplay();
+}
+
+function menuDisplay(){
+    var nowMenuView = document.getElementById('ready_menu_wrapper');
+    nowMenuView.style.display = 'block';
+}
+
+function festaInfoScroll(){
+    var scrollDiv = document.getElementById('festa_info');
+    var maxHeight = scrollDiv.clientHeight;
+    scrollDiv.style.height = maxHeight + 'px';
+}
